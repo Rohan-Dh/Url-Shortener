2f96e36880d56c5ec36c1e2cd9241c0a5b0f3bbc
iamdhrohan <rohandhungana2002@gmail.com>
2026-02-01 13:24:08 +0545

    database switch to postgreSql

--- UrlShortner/settings.py
+++ UrlShortner/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -61,11 +62,17 @@ WSGI_APPLICATION = 'UrlShortner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")  # set this in Vercel env vars
+    )
 }
 
 
