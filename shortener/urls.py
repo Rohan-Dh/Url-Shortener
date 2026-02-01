508c20b840d082aa52a515f4095301ebc1b5f70e
iamdhrohan <rohandhungana2002@gmail.com>
2026-02-01 15:02:54 +0545

    removed redirect_short_url

--- shortener/urls.py
+++ shortener/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('edit/<int:pk>/', edit_short_url, name='edit'),
     path('delete/<int:pk>/', delete_short_url, name='delete'),
     
-    path("<str:code>/", redirect_short_url, name="redirect_short_url"),
+    # path("<str:code>/", redirect_short_url, name="redirect_short_url"),
     # new path added
     path('public-qr/<str:code>/', public_qr_code_png, name='public-qr'),
     path('auth-qr/<str:code>/', auth_qr_code_png, name='auth-qr'),
